21e9c8b852e1580549508fb37b458c7fb94ea372
Jonas Wippermann <jonas@vfuc.co>
2015-09-12 09:18:10 +0300

    Adding StackGrid properties + types

--- StackGrid/StackGrid.swift
+++ StackGrid/StackGrid.swift
@@ -9,10 +9,47 @@
 import UIKit
 
 
+/*
+//MARK: Types
+*/
+
+struct TreeElement {
+    var type: TreeElementType
+    var child1ViewIndex, child2ViewIndex : Int?
+    var set = false
+    
+    init(type: TreeElementType){
+        self.type = type
+    }
+    
+}
+
+enum TreeElementType{
+    case Node
+    case Leaf
+}
+
+
+
+
+
+
+
 class StackGrid : UIView {
     
+    /*
+    //MARK: Properties
+    */
+    
+    let rootNode: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .Vertical
+        stackView.distribution = .FillEqually
+        return stackView
+        }()
+    
+    var tree = [TreeElement]()
+    var views = [UIView]()
     
     
-    
-
 }
