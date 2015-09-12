859f97420a27155614b8a7a1b000cf4e5271f151
Jonas Wippermann <jonas@vfuc.co>
2015-09-12 09:09:45 +0300

    Initial class setup, Extension

---
+++ StackGrid/StackGrid-Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  StackGrid-Extensions.swift
+//  StackGrid
+//
+//  Created by Jonas on 12/09/15.
+//  Copyright © 2015 VFUC. All rights reserved.
+//
+
+import UIKit
+
+extension UILayoutConstraintAxis{
+    
+    //Returns the (logical) opposite of an axis
+    func inverse() -> UILayoutConstraintAxis{
+        
+        switch self {
+            
+        case .Horizontal:
+            return .Vertical
+            
+        case .Vertical:
+            return .Horizontal
+        }
+    }
+}
---
+++ StackGrid/StackGrid.swift
@@ -0,0 +1,18 @@
+//
+//  StackGrid.swift
+//  StackGrid
+//
+//  Created by Jonas on 12/09/15.
+//  Copyright © 2015 VFUC. All rights reserved.
+//
+
+import UIKit
+
+
+class StackGrid : UIView {
+    
+    
+    
+    
+
+}
